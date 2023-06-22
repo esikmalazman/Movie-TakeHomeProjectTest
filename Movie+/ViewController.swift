22df564876cc615ad90f8eeb2b16d433ac3de0bb
Ikmal Azman <opikmalazman@gmail.com>
2023-06-22 18:20:52 +0100

    Initial Commit

---
+++ Movie+/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie+
+//
+//  Created by Ikmal Azman on 22/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
