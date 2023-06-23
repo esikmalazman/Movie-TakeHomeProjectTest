2ae305238a63144462ddb4a8ff444401c555dd3b
Ikmal Azman <opikmalazman@gmail.com>
2023-06-23 18:32:41 +0100

    add test cases to validate searchbar and table view UI state

--- Movie+Tests/MovieSearchVCTests.swift
+++ Movie+Tests/MovieSearchVCTests.swift
@@ -3,7 +3,39 @@ import XCTest
 
 final class MovieSearchVCTests: XCTestCase {
 
-    func test_zero() throws {
-        XCTFail("Tests not yet implemented in MovieSearchVCTests")
+    var sut : MovieSearchVC!
+    
+    override func setUp() {
+        super.setUp()
+        sut = MovieSearchVC()
+        sut.loadViewIfNeeded()
+    }
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+    
+    func test_outletsShouldBeConnected() {
+        XCTAssertNotNil(sut.searchResultsTableView, "searchResultsTableView")
+        XCTAssertNotNil(sut.searchBar, "searchBar")
+    }
+    
+    func test_delegatesShouldBeConnected() {
+        XCTAssertNotNil(sut.searchBar.delegate, "searchBar delegate")
+        XCTAssertNotNil(sut.searchResultsTableView.delegate, "searchResultsTableView delegate")
+        XCTAssertNotNil(sut.searchResultsTableView.dataSource, "searchResultsTableView dataSource")
+    }
+    
+    func test_sutTitle_shouldBeSearch() {
+        XCTAssertEqual(sut.title, "Search")
+    }
+    
+    func test_searchBarPlaceholder_shouldBeSearchForMovies() {
+        XCTAssertEqual(sut.searchBar.placeholder, "Search for Movies")
+    }
+    
+    func test_searchBarCancelButton_shouldEnabled() {
+        XCTAssertTrue(sut.searchBar.showsCancelButton, "showsCancelButton")
     }
 }
