33e0f60b6eeea92d1974b142ea12f81552ee9261
Ikmal Azman <opikmalazman@gmail.com>
2023-06-23 18:23:30 +0100

    add unit test bundle into app

---
+++ Movie+Tests/MovieSearchVCTests.swift
@@ -0,0 +1,9 @@
+@testable import Movie_
+import XCTest
+
+final class MovieSearchVCTests: XCTestCase {
+
+    func test_zero() throws {
+        XCTFail("Tests not yet implemented in MovieSearchVCTests")
+    }
+}
