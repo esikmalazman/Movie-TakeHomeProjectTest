bd99dc2979230d103cbedf389315733eda0c4213
Ikmal Azman <opikmalazman@gmail.com>
2023-06-24 21:02:11 +0100

    create data model and add stubs for fetch movie list and details

---
+++ Movie+/Remote/Model/Movie.swift
@@ -0,0 +1,26 @@
+//
+//  Movie.swift
+//  Movie+
+//
+//  Created by Ikmal Azman on 24/06/2023.
+//
+
+import Foundation
+
+struct MovieListResponse : Codable {
+    let results : [Movie]?
+}
+
+struct Movie : Codable {
+    let id : Int?
+    let title : String?
+    let releaseDate : String?
+    let posterPath : String?
+}
+
+struct MovieDetail : Codable {
+    let title : String?
+    let overview : String?
+    let releaseDate : String?
+    let backdropPath : String?
+}
