0348797d3acdf2718e8859dff1ba7bc98eae47b1
Ikmal Azman <opikmalazman@gmail.com>
2023-06-26 23:41:23 +0100

    design movie detail screen

--- Movie+/MovieDetail/MovieDetailVC.swift
+++ Movie+/MovieDetail/MovieDetailVC.swift
@@ -10,13 +10,27 @@ import UIKit
 #warning("""
 Todo's
 1. Display selected movie from search results
-- Setup UI for Movie Information
+- Setup UI for Movie Information ✅
 - Add option to save movie in favourites, save through Core Data (Bonus)
 """)
 
 final class MovieDetailVC: UIViewController {
     
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var movieReleaseDateTitle: UILabel!
+    @IBOutlet weak var overviewDescription: UILabel!
+    @IBOutlet weak var addToFavouriteBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        posterImageView.layer.cornerRadius = 10
+        addToFavouriteBtn.layer.cornerRadius = 10
+    }
+}
+
+// MARK:  Actions
+extension MovieDetailVC {
+    @IBAction func addToFavouriteTapped(_ sender: UIButton) {
     }
 }
