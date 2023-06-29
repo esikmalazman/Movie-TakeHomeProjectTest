2854fd9263add350b6d59741749f66d1af5b701a
Ikmal Azman <opikmalazman@gmail.com>
2023-06-29 07:50:11 +0100

    setup initial view for recentsSearchVC

--- Movie+/Screens/RecentlySearch/RecentsSearchVC.swift
+++ Movie+/Screens/RecentlySearch/RecentsSearchVC.swift
@@ -8,10 +8,56 @@
 import UIKit
 
 final class RecentsSearchVC: UIViewController {
-
+    
+    @IBOutlet weak var recentsTableView : UITableView!
+    
+    let testCell = "testCell"
+    
+    var movieQueries = [MovieQuery]()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+#warning("move to presenter")
+        AppDelegate.shared.cdStack.fetchContext(of: MovieQuery.self, with: []) { result in
+            self.movieQueries = result
+            DispatchQueue.main.async {
+                self.recentsTableView.reloadData()
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Recents Search"
+        configureRecentsTableView()
     }
+}
 
+extension RecentsSearchVC : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movieQueries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: testCell, for: indexPath)
+        cell.textLabel?.text = movieQueries[indexPath.row].query
+        return cell
+    }
+}
+
+extension RecentsSearchVC : UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
+
+private extension RecentsSearchVC {
+    func configureRecentsTableView() {
+        title = "Recents Search"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        recentsTableView.register(UITableViewCell.self, forCellReuseIdentifier: testCell)
+        recentsTableView.delegate = self
+        recentsTableView.dataSource = self
+    }
 }
