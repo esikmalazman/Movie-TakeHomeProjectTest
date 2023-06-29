a16e55a61f3cba9c90d4741cd658635d8b3097b7
Ikmal Azman <opikmalazman@gmail.com>
2023-06-29 09:45:22 +0100

    setup UI and layout data for selected recent query result screen

---
+++ Movie+/Screens/RecentsDetail/RecentDetailVC.swift
@@ -0,0 +1,53 @@
+//
+//  RecentDetailVC.swift
+//  Movie+
+//
+//  Created by Ikmal Azman on 29/06/2023.
+//
+
+import UIKit
+
+final class RecentDetailVC: UIViewController {
+
+    @IBOutlet weak var moviesListTableView : UITableView!
+    
+    var movieList : [Movie] = []
+    var recentsQueryTitle : String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureMovieListTableView()
+    }
+    
+    func setMovieList(_ data : [Movie], _ title : String) {
+        recentsQueryTitle = title
+        movieList = data
+    }
+}
+
+extension RecentDetailVC : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movieList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let movie = movieList[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: MovieResultCell.identifier, for: indexPath) as! MovieResultCell
+        cell.configure(movie)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Recently search result for : \(recentsQueryTitle)"
+    }
+}
+
+private extension RecentDetailVC {
+    func configureMovieListTableView() {
+        moviesListTableView.register(MovieResultCell.nib(), forCellReuseIdentifier: MovieResultCell.identifier)
+        
+        moviesListTableView.dataSource = self
+        moviesListTableView.reloadData()
+    }
+}
